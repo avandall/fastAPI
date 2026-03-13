0201d031814dbe800413a30d946319548a2f0a87
unknown <avannguyen.nina@gmail.com>
2026-03-13 14:59:01 +0700

    fix bug railway

--- app/config.py
+++ app/config.py
@@ -10,6 +10,10 @@ class Settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
+        )
 
 settings = Settings()
